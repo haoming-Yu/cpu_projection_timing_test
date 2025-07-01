0d1a0ff7e0ef5b1b97407898dd5d68d77424481f
haoming-Yu <1813442316@qq.com>
2025-07-01 13:27:59 +0800

    cpu version of projection time test

---
+++ README.md
@@ -0,0 +1,2 @@
+# CPU Projection test
+too slow for praticle use, check for more information and analyse in this repo: 
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <chrono>
 #include <Eigen/Eigen>
 #include <Eigen/Dense>
 #include "mesh.h"
@@ -19,11 +20,11 @@ void project_vertexes_to_cameras(std::vector<float3>& vertexes, Camera::Cam& cam
             if (vertex_cam(2) > 0) {
                 // std::cout << "vertex_cam_3d: " << vertex_cam_3d.x << " " << vertex_cam_3d.y << " " << vertex_cam_3d.z << std::endl;
                 vertexes_cam[i].push_back(vertex_cam_3d);
-                if (j < 100000) {
-                    std::cout << "vertex " << j << " visible to camera " << i << " " << vertex.x << " " << vertex.y << " " << vertex.z  << std::endl << "camera extrinsic: " << cam.extrinsics_[i].T_cw.matrix() << std::endl;
-                } else {
-                    break;
-                }
+                // if (j < 100000) {
+                //     std::cout << "vertex " << j << " visible to camera " << i << " " << vertex.x << " " << vertex.y << " " << vertex.z  << std::endl << "camera extrinsic: " << cam.extrinsics_[i].T_cw.matrix() << std::endl;
+                // } else {
+                //     break;
+                // }
             }
         }
     }
@@ -42,28 +43,36 @@ void project_cameras_to_uv(std::vector<std::vector<float3>>& vertexes_cam, Camer
                 // std::cout << "uv: " << u << " " << v << " visible to camera " << i << std::endl;
             }
         }
-        std::cout << "Vertexes number projected to camera " << i << " (visible to camera " << i << "): " << uv_cam[i].size() << std::endl;
+        // std::cout << "Vertexes number projected to camera " << i << " (visible to camera " << i << "): " << uv_cam[i].size() << std::endl;
     }
 }
 
 int main() {
     MeshProcessing::Mesh mesh;
-    mesh.loadFromFile("../data/desk/mesh/optimized_mesh.ply");
+    mesh.loadFromFile("../data/underground/mesh/filtered_mesh.ply");
     std::vector<float3> vertexes = mesh.getVertexes();
     std::cout << "Vertexes number rechecked: " << vertexes.size() << std::endl;
 
     Camera::Cam cam;
-    cam.loadIntrinsic("../data/desk/intrinsic.log");
-    cam.loadExtrinsics("../data/desk/traj.log");
+    cam.loadIntrinsic("../data/underground/intrinsic.log");
+    cam.loadExtrinsics("../data/underground/traj.log");
     cam.dump_intrinsic_to_float(); // prepare for the data loading of gpu
     cam.dump_extrinsic_to_float();
 
+    auto start_time = std::chrono::high_resolution_clock::now();
     std::vector<std::vector<float3>> vertexes_cam;
     project_vertexes_to_cameras(vertexes, cam, vertexes_cam);
+    auto end_time = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> duration = end_time - start_time;
+    std::cout << "Time taken to transform vertexes to cameras: " << duration.count() << " seconds" << std::endl;
     // std::cout << "Vertexes number projected to camera 0 (infront of camera 0): " << vertexes_cam[0].size() << std::endl;
 
+    start_time = std::chrono::high_resolution_clock::now();
     std::vector<std::vector<float2>> uv_cam;
     project_cameras_to_uv(vertexes_cam, cam, uv_cam);
+    end_time = std::chrono::high_resolution_clock::now();
+    duration = end_time - start_time;
+    std::cout << "Time taken to project cameras to uv: " << duration.count() << " seconds" << std::endl;
     // std::cout << "Vertexes number projected to camera 0 (infront of camera 0): " << uv_cam[0].size() << std::endl;
 
     return 0;
